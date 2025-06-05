2c7cb9283c33d074a43ca37d8250ac180e6dc74b
burakfaydali <87203521+burakfaydali@users.noreply.github.com>
2025-06-05 19:26:25 +0300

    Initial commit

---
+++ src/main/kotlin/com/bfaydali/bookinventory/BookinventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.bfaydali.bookinventory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookinventoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookinventoryApplication>(*args)
+}
